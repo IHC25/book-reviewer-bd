761664a9ff99d0570966c3b9f845179c211ba1c8
arefin6 <imranhchy@gmail.com>
2022-04-04 11:04:13 +0600

    title and image added in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Home.css";
+import homeImage from "../../images/book.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h2>My home</h2>
+    <div className="home">
+      <div className="home-info-container">
+        <h1>Read The Best Books</h1>
+        <p>
+          Our website contains best book reviews. Here you can get the real
+          reviews of books from book readers all around the world. So, our
+          website made it easy to find the best books to read. You're in the
+          right place. Tell us what titles or genres you've enjoyed in the past,
+          and we'll give you surprisingly insightful recommendations. Explore
+          and Enjoy.
+        </p>
+      </div>
+      <div className="home-image-container">
+        <img src={homeImage} alt="" />
+      </div>
     </div>
   );
 };
