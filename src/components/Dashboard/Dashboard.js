c2d3f429e026aa0931dccb2f28d5adc1afc21390
arefin6 <imranhchy@gmail.com>
2022-04-04 13:48:14 +0600

    charts added in dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./Dashboard.css";
 
 const Dashboard = () => {
+  const [chart, setChart] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setChart(data));
+  }, []);
+  console.log(chart);
   return (
     <div>
-      <h2>My Dashboard</h2>
+      <div className="charts">
+        <div className="chart-style">
+          <h3>Sales</h3>
+          <BarChart width={500} height={350} data={chart}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="sell" fill="#8884d8" />
+          </BarChart>
+        </div>
+        <div className="chart-style">
+          <h3>Investment and Revenue</h3>
+          <LineChart
+            width={500}
+            height={350}
+            data={chart}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+            <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+          </LineChart>
+        </div>
+      </div>
     </div>
   );
 };
